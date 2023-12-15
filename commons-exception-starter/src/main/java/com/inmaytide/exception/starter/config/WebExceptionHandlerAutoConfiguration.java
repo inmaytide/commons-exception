7f364b3d2a61e5bf48435527c821c847fee4097e
inmaytide <inmaytide@gmail.com>
2023-12-15 16:19:59 +0800

    Upgrade jdk version to  21 && upgrade dependencies

--- commons-exception-starter/src/main/java/com/inmaytide/exception/starter/config/WebExceptionHandlerAutoConfiguration.java
+++ commons-exception-starter/src/main/java/com/inmaytide/exception/starter/config/WebExceptionHandlerAutoConfiguration.java
@@ -11,7 +11,6 @@ import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean
 import org.springframework.boot.autoconfigure.condition.ConditionalOnWebApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Import;
 
 /**
  * @author inmaytide
